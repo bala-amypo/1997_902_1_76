6f26739fd4eaccbdbbb271600e034ec53309eec4
bala-amypo <bala@amypo.in>
2025-12-19 09:13:42 +0000

    User commit

--- src/main/java/com/example/demo/Entity/PersonProfile.java
+++ src/main/java/com/example/demo/Entity/PersonProfile.java
@@ -56,5 +56,7 @@ public class PersonProfile{
     public Boolean getrelationshipDeclared(){
         return relationshipDeclared;
     }
-    public void 
+    public void setrelationshipDeclared(){
+        
+    }
 }
