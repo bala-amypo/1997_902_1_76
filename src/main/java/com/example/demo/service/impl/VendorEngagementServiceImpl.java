fb5f0192d605a047748533d1312b03a725e31ae1
bala-amypo <bala@amypo.in>
2025-12-26 17:57:44 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorEngagementServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorEngagementServiceImpl.java
@@ -1,3 +1,14 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.VendorEngagementRecord;
+import com.example.demo.repository.VendorEngagementRepository;
+import com.example.demo.service.VendorEngagementService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class VendorEngagementServiceImpl implements VendorEngagementService {
 
